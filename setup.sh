9c2a044faae3cee1cd665829363929d3b2c848d7
aborgardt <alexander.borgardt@gmail.com>
2024-06-01 16:07:05 +0200

    install ansible-galaxy stuff

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@
 
 sudo apt-get update -y
 sudo apt-get install -y ansible python3-pip wget curl unzip git
+
+sudo ansible-galaxy role install -r roles/requirements.yml --force
+sudo ansible-galaxy collection install -r roles/requirements.yml --force
